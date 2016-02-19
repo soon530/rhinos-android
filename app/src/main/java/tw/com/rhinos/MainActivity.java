5208eafc7d8c474d2de952dba38647eb06b10a04
vic <soon530@gmail.com>
2016-02-19 13:49:11 +0800

    Done! the menu feature.

--- app/src/main/java/tw/com/rhinos/MainActivity.java
+++ app/src/main/java/tw/com/rhinos/MainActivity.java
@@ -2,9 +2,8 @@ package tw.com.rhinos;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,22 +81,36 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        switch (id) {
+            case R.id.nav_point:
+                break;
 
-        } else if (id == R.id.nav_slideshow) {
+            case R.id.nav_award:
+                break;
 
-        } else if (id == R.id.nav_manage) {
+            case R.id.nav_store:
+                break;
 
-        } else if (id == R.id.nav_share) {
+            case R.id.nav_group:
+                showTaost();
+                break;
 
-        } else if (id == R.id.nav_send) {
+            case R.id.nav_card:
+                showTaost();
+                break;
 
+            case R.id.nav_find:
+                showTaost();
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void showTaost() {
+        Toast toast = Toast.makeText(this, "正在開發中...", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
